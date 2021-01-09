d73b4ca918b890fefd3d66f2b41e8635ac861e67
Cubified <cubedoodldev@gmail.com>
2021-01-08 21:28:59 -0500

    Move truecolor_stresstest.c into test/ folder

--- test/truecolor_stresstest.c
+++ test/truecolor_stresstest.c
@@ -15,11 +15,16 @@ int main(){
   struct winsize ws;
 
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+
+  printf("\x1b[?25l\n");
+
   for(y=0;y<ws.ws_row;y++){
     for(x=0;x<ws.ws_col;x++){
       printf("\x1b[48;2;%i;%i;%im ", (int)floor(((double)y/(double)ws.ws_row)*255.0f), (int)floor(((double)x/(double)ws.ws_col)*255.0f), (int)floor(((double)y/(double)ws.ws_row)*255.0f));
     }
   }
+
+  printf("\x1b[0m\x1b[?25h\x1b[0;0H\n");
 
   return 0;
 }
